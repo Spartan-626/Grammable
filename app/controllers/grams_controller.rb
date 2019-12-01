4fed0a91b0f37610e00529efce9b479e692ca7e3
Spartan-626 <kwilliamson0232@gmail.com>
2019-12-01 22:54:02 +0000

    Can now edit and update grams.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,28 +1,44 @@
 class GramsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create]
-
+  
     def new
       @gram = Gram.new
     end
   
     def index
     end
-
+  
     def show
-        @gram = Gram.find_by_id(params[:id])
-        if @gram.blank?
-            render plain: 'Not Found :(', status: :not_found
-        end
+      @gram = Gram.find_by_id(params[:id])
+      return render_not_found if @gram.blank?
+    end
+  
+    def edit
+      @gram = Gram.find_by_id(params[:id])
+      return render_not_found if @gram.blank?
+    end
+
+    def update
+      @gram = Gram.find_by_id(params[:id])
+      return render_not_found if @gram.blank?
+
+      @gram.update_attributes(gram_params)
+      
+      if @gram.valid?
+        redirect_to root_path
+      else
+        return render :edit, status: :unprocessable_entity
+      end
     end
   
     def create
-        @gram = current_user.grams.create(gram_params)
-        if @gram.valid?
-          redirect_to root_path
-        else
-          render :new, status: :unprocessable_entity
-        end
+      @gram = current_user.grams.create(gram_params)
+      if @gram.valid?
+        redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
       end
+    end
   
     private
   
@@ -30,4 +46,7 @@ class GramsController < ApplicationController
       params.require(:gram).permit(:message)
     end
   
+    def render_not_found
+      render plain: 'Not Found :(', status: :not_found
+    end
   end
