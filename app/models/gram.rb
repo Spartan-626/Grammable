671bbaea8e103ded311dd1c9511b06a434abc22c
Spartan-626 <kwilliamson0232@gmail.com>
2019-12-03 04:48:12 +0000

    Trying to fix undefined method picture_will_change!

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -2,7 +2,7 @@ class Gram < ApplicationRecord
     validates :message, presence: true
     validates :picture, presence: true
 
-    #mount_uploader :picture, PictureUploader
+    mount_uploader :picture, PictureUploader
 
     belongs_to :user
 end
